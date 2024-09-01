d44b4134ba6a0d05093b020711e2459b50a111ec
Neytred <ls.ivanova91@gmail.com>
2024-09-01 17:47:02 +0900

    Homework 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+//        final Bicycle b1 = new Bicycle("b1", 2);
+//        final Bicycle b2 = new Bicycle("b2", 2);
+//        final Car c1 = new Car("c1", 4);
+//        final Car c2 = new Car("c2", 4);
+        final Truck t1 = new Truck("t1", 8, new SmallTrailer());
+        final Truck t2 = new Truck("t2", 8, new BigTrailer());
 
+        final Checkable[] wheeledTransports = {t1, t2};
+        for (Checkable wheeledTransport : wheeledTransports) {
+            wheeledTransport.check();
+        }
     }
 }
